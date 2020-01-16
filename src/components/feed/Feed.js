9a61799b8ba7928bdcadd35a257d8ec300265744
200backflips <fredrik.karlsson@appliedtechnology.se>
2020-01-16 23:03:16 +0100

    jk, NOW is the last push of the day

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './Feed.css';
 import { SentMessage } from '../sentmessage/SentMessage';
@@ -12,13 +12,29 @@ import {
 
 export const Feed = () => {
 	const { username } = useSelector(state => state.loginReducer);
+	const { adminMessages } = useSelector(state => state.messageReducer);
 	const { sentMessages } = useSelector(state => state.messageReducer);
 	const { receivedMessages } = useSelector(state => state.messageReducer);
-	const { adminMessages } = useSelector(state => state.messageReducer);
 	const dispatch = useDispatch();
+
+	const [displayMessages, setDisplayMessages] = useState([]);
 
 	let socket;
 	const ENDPOINT = 'localhost:8080';
+
+	const handleSentMessage = () => {
+		setDisplayMessages(displayMessages => [
+				...displayMessages,
+				<SentMessage content={sentMessages[sentMessages.length - 1]} />
+			]);
+	};
+
+	const handleReceivedMessage = () => {
+		setDisplayMessages(displayMessages => [
+			...displayMessages,
+			<ReceivedMessage content={receivedMessages[receivedMessages.length - 1]} />
+		]);
+};
 
 	useEffect(() => {
 		socket = io(ENDPOINT);
@@ -28,26 +44,22 @@ export const Feed = () => {
 	}, [ENDPOINT, username]);
 
 	useEffect(() => {
+		handleSentMessage();
+	}, [sentMessages]);
+	
+	useEffect(() => {
 		socket = io(ENDPOINT);
 
 		socket.on('receivedMessages', message => {
 			dispatch(appendReceivedMessage(message));
 		});
-	}, [ENDPOINT, receivedMessages]);
+		handleReceivedMessage();
+	}, [sentMessages]);
+
 
 	return (
 		<div className="Feed">
-			<div className="content">
-				{adminMessages.map((content, i) => (
-					<AdminMessage content={content} key={i} />
-				))}
-				{receivedMessages.map((content, i) => (
-					<ReceivedMessage content={content} key={i} />
-				))}
-				{sentMessages.map((content, i) => (
-					<SentMessage content={content} key={i} />
-				))}
-			</div>
+			<div className="content">{displayMessages.length > 1 && displayMessages}</div>
 		</div>
 	);
 };
