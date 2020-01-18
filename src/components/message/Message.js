578703fd91e670a67fb56f8576e096529957b8cf
200backflips <fredrik.karlsson@appliedtechnology.se>
2020-01-18 22:44:02 +0100

    increase responsivity

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -2,20 +2,12 @@ import React from 'react';
 import './Message.css';
 
 export const Message = ({ content, styling }) => {
-	// const parseTimestamp = input => {
-	// 	const regex1 = /[\d.:-]+/gi;
-	// 	const regex2 = /[\w:]+/gi;
-	// 	let splitDate = input.match(regex1)
-	// 	return `${splitDate[0]} ${splitDate[1].match(regex2)[0]}`;
-	// };
-
 	return (
 		<div className="Message">
 			<p className={`user${styling}`}>{content.user}</p>
 			<div className={`speech-bubble${styling}`}>
 				<p>{content.message}</p>
 			</div>
-			{/* <span className="timestamp">{parseTimestamp(content.timestamp)}</span> */}
 		</div>
 	);
 };
